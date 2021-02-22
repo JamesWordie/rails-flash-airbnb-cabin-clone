ac7922ae9b57303ec68f467fb4c15c2499f21244
matt vile <matt.vile@yahoo.co.uk>
2021-02-22 16:43:48 +0000

    sorting git murge issue

--- app/models/cabin.rb
+++ app/models/cabin.rb
@@ -1,3 +1,14 @@
 class Cabin < ApplicationRecord
 
+belongs_to :user
+has_many :bookings
+
+# Validate presence and uniqueness for each field
+validates :name, presence: true, uniqueness: true
+# validates :description, presence: true, uniqueness: true
+# validates :price_per_night, presence: true, uniqueness: true
+# validates :number_of_guests, presence: true, uniqueness: true
+# validates :location, presence: true, uniqueness: true
+validates :user_id, presence: true,
+
 end
