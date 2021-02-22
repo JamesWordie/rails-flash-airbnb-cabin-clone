b9c8faeee96c560e878caed59680a9d3ce3b5ac0
Jonathan Simpson <jonathansimpson101@gmail.com>
2021-02-22 15:57:48 +0000

    removed the validations on the booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,16 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :cabin
-
-  # Validate presence for each field
-  validates :user, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :cabin_id, presence: true
-
-  # Validate presence for each field
-  validates :user, uniqueness: true
-  validates :start_date, uniqueness: true
-  validates :end_date, uniqueness: true
-  validates :cabin_id, uniqueness: true
 end
