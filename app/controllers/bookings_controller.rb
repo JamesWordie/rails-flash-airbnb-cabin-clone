6db9629d4ef02ad5c50f977e74e81f88299abc1a
matt vile <matt.vile@yahoo.co.uk>
2021-02-23 17:27:25 +0000

    add delete action to bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,10 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+  def delete
+    @booking = Booking.find(params:[id])
+    @booking.destroy
+
+    redirect_to booking_path
+  end
 end
