226675313542cead27d5a5d146c2884ecd4a6c2b
Jonathan Simpson <jonathansimpson101@gmail.com>
2021-02-23 16:20:34 +0000

    added the create method to the bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,25 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+
+  def create
+    @booking = Booking.new(strong_params)
+    @booking.user = current_user
+    cabin = Cabin.find(params[:cabin_id])
+    @booking.cabin = cabin
+
+    if @booking.valid?
+      @booking.save
+      redirect_to cabins_path
+    else
+      render 'cabins/show'
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
 end
