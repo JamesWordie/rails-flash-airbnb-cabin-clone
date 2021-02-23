70e924be085d762d80abf39e060b47bd2eb506a8
James Wordie <james.wordie@yahoo.com>
2021-02-23 15:48:24 +0000

    chnage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: ‘pages#home’
+  root to: 'pages#home'
   resources :cabins do #show index without log-in
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, except: [:new, :create, :index]
-  get ‘profile’, to: ‘pages#profile’
+  get ‘profile’, to: 'pages#profile'
 end
 
 
