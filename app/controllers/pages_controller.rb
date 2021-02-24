5452ee607a8a06f6d4fb798a26163118fe18285d
Jonathan Simpson <jonathansimpson101@gmail.com>
2021-02-24 14:03:59 +0000

    homepage styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @cabins = Cabin.all
+    @selected_cabins = @cabins.first(8)
   end
 
   def profile
