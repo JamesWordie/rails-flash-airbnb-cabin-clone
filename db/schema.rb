271efa7ca0bf4f90e5aba4f82983e9dd1d894613
James Wordie <james.wordie@yahoo.com>
2021-02-25 16:10:25 +0000

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_143512) do
-ActiveRecord::Schema.define(version: 2021_02_25_110853) do
-ActiveRecord::Schema.define(version: 2021_02_25_110406) do
 ActiveRecord::Schema.define(version: 2021_02_25_112639) do
 
   # These are extensions that must be enabled in order to support this database
