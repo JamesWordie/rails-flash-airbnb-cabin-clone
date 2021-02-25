c684bdfa25b3b8b160d9319bfb1ce50bffbc6a0c
James Wordie <james.wordie@yahoo.com>
2021-02-25 13:38:59 +0000

    completed map index page

--- app/controllers/cabins_controller.rb
+++ app/controllers/cabins_controller.rb
@@ -4,7 +4,8 @@ class CabinsController < ApplicationController
     @markers = @cabins.geocoded.map do |cabin|
       {
         lat: cabin.latitude,
-        long: cabin.longitude
+        lng: cabin.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { cabin: cabin })
       }
     end
   end
@@ -12,6 +13,10 @@ class CabinsController < ApplicationController
   def show
     @booking = Booking.new
     @cabin = Cabin.find(params[:id])
+    # @markers = [{
+    #   lat: @cabin.latitude,
+    #   long: @cabin.longitude
+    # }]
   end
 
   def new
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,11 +20,11 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
-import { initMapbox } from '../plugins/init_mapbox';
 
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
