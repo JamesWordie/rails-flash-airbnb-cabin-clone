8ec4fb7952be656bd60b262e2a874e7d23ff0673
Jonathan Simpson <jonathansimpson101@gmail.com>
2021-02-25 11:20:41 +0000

    added reviews with associations and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_175143) do
+ActiveRecord::Schema.define(version: 2021_02_25_110853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,17 @@ ActiveRecord::Schema.define(version: 2021_02_22_175143) do
     t.index ["user_id"], name: "index_cabins_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,4 +66,6 @@ ActiveRecord::Schema.define(version: 2021_02_22_175143) do
   add_foreign_key "bookings", "cabins"
   add_foreign_key "bookings", "users"
   add_foreign_key "cabins", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users"
 end
