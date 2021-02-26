b3ebe5ec93501933f1a31137d8377bf10ef84f95
matt vile <matt.vile@yahoo.co.uk>
2021-02-26 13:02:11 +0000

    header style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,7 +27,6 @@ import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 import { initMapboxShow } from '../plugins/init_mapbox_show';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
